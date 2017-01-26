2f45163b39cb0d8b10d461ed3da753ca61a32788
atonusgit <anton@valle.fi>
2017-01-26 16:51:51 +0200

    Add files via upload

---
+++ CyberChat/sql/user_database-import.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (usr, pw) VALUES ('Cyber Admin', 'admin');
+INSERT INTO users (usr, pw) VALUES ('Lol man', 'password');
+INSERT INTO users (usr, pw) VALUES ('Batman', 'Robin');
+INSERT INTO users (usr, pw) VALUES ('user', 'password');
---
+++ CyberChat/sql/user_database-schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (
+    usr varchar(50) PRIMARY KEY,
+    pw varchar(50)
+);
